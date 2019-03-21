033647574d0998777242655893bf6ae10e5386e5
xzxcuzme <mutantick@gmail.com>
2019-03-22 02:30:39 +0700

    1

--- 2. csv writer, reader/src/csv.java
+++ 2. csv writer, reader/src/csv.java
@@ -1,27 +1,17 @@
-import com.opencsv.CSVWriter;
-
 import java.io.*;
 
 public class csv {
 
     public static void main(String[] args) {
-
+        String newcsv = "C:/Users/Игшорь/Documents/GitHub/java/2. csv writer, reader/src/dataout.csv";
         try(FileReader reader = new FileReader("C:/Users/Игшорь/Documents/GitHub/java/2. csv writer, reader/src/data.csv"))
         {
             int c;
-
-            String newcsv = "C:/Users/Игшорь/Documents/GitHub/java/2. csv writer, reader/src/dataout.csv";
             PrintWriter writer = new PrintWriter(new FileWriter(newcsv));
-
-
-
             while((c=reader.read())!=-1){
                 System.out.print((char)c);
-                writer.println("Hllo worl!");
-               // String [] country = "India#China#United States".split("#");
-                //writer.writeNext(country);
-                //writer.close();
-            }
+                writer.println((char)c + "           ");
+             }
 
         }
         catch(IOException ex){
