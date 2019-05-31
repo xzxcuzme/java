f74d4d7cf6bd11cf89d17c56a4008531dee4bfee
Илья Тришин <trishin.ilija@gmail.com>
2019-06-01 01:00:22 +0700

    Revert "Revert "g""
    This reverts commit 91b17a064f1be3c6a20574f5aeccfcf01dd9e2d7.

--- demo/src/main/java/com/example/demo/HTML.java
+++ demo/src/main/java/com/example/demo/HTML.java
@@ -1,14 +1,12 @@
 package com.example.demo;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
-
 @Controller
-
 public class HTML {
-    @GetMapping("/info.html")
+    @RequestMapping("/index.html")
     public String info() {
-        return "info.html";
+        return "index.html";
     }
 }
--- demo/src/main/java/com/example/demo/User.java
+++ demo/src/main/java/com/example/demo/User.java
@@ -1,21 +1,59 @@
 package com.example.demo;
 
 
-public class User {
-    private Integer id;
-    private String FIO;
-    private String phone;
+//import org.apache.log4j.Logger;
 
-    public void setId (Integer id){
-        this.id=id;
+/**
+ * Класс пользователь.
+ * @author Илья Тришин i@gmail.com
+ * @version 1.0
+ */
+
+public class User {
+
+    /** final static Logger logger = Logger.getLogger(User.class);
+    public void fict() {
+        logger.info("Test log");
     }
-    public String toString() {
-        return String.valueOf((this.id)+" "+this.FIO+" "+this.phone);
-    }
+     */
+    //@Id
+    private Integer id;
+
+    private String FIO;
+
+    private String phone;
 
     public User(Integer id, String FIO, String phone) {
         this.id = id;
         this.FIO = FIO;
         this.phone = phone;
     }
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    /**@Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s, FIO='%s', phone='%s']",
+                id, FIO, phone);
+    }
+
+     */
+    /** public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return String.valueOf((this.id)+" "+this.FIO+" "+this.phone);
+    }
+     */
 }
