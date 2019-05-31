a6af057ab410f9306a3ba7884bd335251b259934
Илья Тришин <trishin.ilija@gmail.com>
2019-06-01 00:56:30 +0700

    g

--- chat/src/main/java/com/ru/chat/ChatApplication.java
+++ chat/src/main/java/com/ru/chat/ChatApplication.java
@@ -1,16 +1,13 @@
-package com.example.demo;
+package com.ru.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
-public class DemoApplication {
+public class ChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ChatApplication.class, args);
 	}
 
 }
-
